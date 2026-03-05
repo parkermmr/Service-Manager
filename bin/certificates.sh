c25aa9b10d6c3b1712b1c752b22c4f0fffc4d998
Parker Rennie <parke@DESKTOP-NFAQ8DH.localdomain>
2026-03-05 16:22:17 +1000

    feat: adds tls + password authentication to metrics.

---
+++ bin/certificates.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+CA_DIRECTORY="$HOME/.prometheus/authority"
+CERTIFICATES="$HOME/.prometheus/certificates"
+mkdir -p $CA_DIRECTORY $CERTIFICATES
+
+echo '
+# Server Certificate Extension File
+# Edit this file to customize your server certificate
+
+basicConstraints                   = CA:FALSE
+nsCertType                         = server
+nsComment                          = "OpenSSL Generated Server Certificate"
+subjectKeyIdentifier               = hash
+authorityKeyIdentifier             = keyid,issuer:always
+keyUsage                           = critical, digitalSignature, keyEncipherment
+extendedKeyUsage                   = serverAuth
+subjectAltName                     = @alt_names
+
+[alt_names]
+# REQUIRED: Minimum 2 DNS names for server certificates
+DNS.1 = localhost
+DNS.2 = localhost
+
+# Optional: Add IP addresses
+IP.1  = 0.0.0.0
+# IP.2  = 10.0.0.50
+' > $CERTIFICATES/server/server.ext
+
+if [[ ! -d "$CA_DIRECTORY" ]] || [[ -z "$(ls -A "$CA_DIRECTORY")" ]]; then
+    CA_DIR="$CA_DIRECTORY" generate-certs --setup-ca
+fi
+
+CA_DIR=$CA_DIRECTORY generate-certs --service server --type server --output $CERTIFICATES/server --cleanup
+CA_DIR=$CA_DIRECTORY generate-certs --service client --type client --output $CERTIFICATES/client --cleanup
